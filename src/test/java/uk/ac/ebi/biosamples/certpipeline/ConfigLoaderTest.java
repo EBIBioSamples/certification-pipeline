3ef06a249e709c56752d8d64c3a604b91c4b76a5
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-11-29 17:05:28 +0000

    added initial pipeline

--- src/test/java/uk/ac/ebi/biosamples/certpipeline/ConfigLoaderTest.java
+++ src/test/java/uk/ac/ebi/biosamples/certpipeline/ConfigLoaderTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.ac.ebi.biosamples.certpipeline.model.Config;
+import uk.ac.ebi.biosamples.certpipeline.service.ConfigLoader;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
--- src/test/java/uk/ac/ebi/biosamples/certpipeline/service/PipelineTest.java
+++ src/test/java/uk/ac/ebi/biosamples/certpipeline/service/PipelineTest.java
@@ -11,34 +11,23 @@ import uk.ac.ebi.biosamples.certpipeline.model.ChecklistMatches;
 import uk.ac.ebi.biosamples.certpipeline.model.Sample;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-public class InterrogatorTest {
+public class PipelineTest {
 
     @Autowired
-    private Interrogator interrogator;
-
-    @Autowired
-    private ConfigLoader configLoader;
+    private Pipeline pipeline;
 
     @Test
-    public void given_ncbi_sample_return_ncbi_checklist_as_a_match() throws IOException {
+    public void given_ncbi_sample_run_pipeline() throws IOException {
         String data = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("json/ncbi-SAMN03894263.json"), "UTF8");
-        Sample sample = new Sample("test-uuid", data);
-        ChecklistMatches checklistMatches = interrogator.interrogate(sample);
+        ChecklistMatches checklistMatches = pipeline.run(data);
+        System.out.println(checklistMatches.toString());
         assertTrue(checklistMatches.getChecklists().size()==1);
         assertEquals("ncbi-0.0.1", checklistMatches.getChecklists().get(0).getID());
-    }
-
-    @Test
-    public void given_empty_sample_return_empty_matches() {
-        Sample sample = new Sample("test-uuid", "{}");
-        ChecklistMatches checklistMatches = interrogator.interrogate(sample);
-        assertEquals(Collections.EMPTY_LIST, checklistMatches.getChecklists());
     }
 }
